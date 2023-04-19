768474ac378848ff2a7f8bdd855c05f13b326340
Casey Bailey <caseybailey@pdx.edu>
2023-04-19 19:29:20 -0400

    Revert "Added Nastify backend service"
    This reverts commit 2b93314f774c36e0188f11763c95af4ee8328fb8.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,29 @@
-/* eslint-disable */
-// @ts-nocheck
-import * as dotenv from 'dotenv';
-import {Nastify} from "./server.js";
+import dotenv from "dotenv";
+import Fastify, {FastifyReply, FastifyRequest} from "fastify";
+import {User} from "./db/entities/User.js";
+import config from "./db/mikro-orm.config.js";
+import {FastifyMikroOrmPlugin} from "./plugins/mikro.js";
+
 dotenv.config();
 
-let app = Nastify();
+const app = Fastify();
 
-app.listen(8080, () => {
-	console.log("Server listening on port 8080...");
-})
+await app.register(FastifyMikroOrmPlugin, config);
+
+app.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+	return 'Hilo';
+}); 
+
+app.get("/dbTest", async (request: FastifyRequest, reply: FastifyReply) => {
+	return request.em.find(User, {});
+});
+
+app.listen({ port: 8080}, 
+	(err, address) => {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+		console.log(`Started server at ${address}`);
+	}
+);
--- backend/src/request.ts
+++
@@ -1,7 +0,0 @@
-import url from "url";
-
-export function request(req) {
-	const parsedUrl = url.parse(`${req.headers.host}${req.url}`, true);
-	const keys = Object.keys(parsedUrl);
-	keys.forEach( (key) => (req[key] = parsedUrl[key]));
-}
